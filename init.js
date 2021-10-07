39ea7bbb64beaf47b38d2d81b650830bc631ec8e
Vladimir <stesh@ukr.net>
2021-10-07 12:19:09 +0300

    add init log

--- init.js
+++ init.js
@@ -25,30 +25,30 @@ const connect = (query) => {
 
 const start = async () => {
 
-    let testRes = connect(test);
-    if(testRes) {
+    const [rows,fields] = connect(test);
+    if(rows) {
         console.log('БД подключена');
-        console.log(testRes)
+        console.log(rows)
     } else {
         console.log('Ошибка подключения к БД')
     }
 
-    const query1 = `CREATE TABLE IF NOT EXISTS users (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        photo VARCHAR(50) NOT NULL,
-        login VARCHAR(50) UNIQUE NOT NULL,
-        password VARCHAR(255) NOT NULL,
-        email VARCHAR(50) UNIQUE NOT NULL,
-        status ENUM ('admin', 'user') DEFAULT 'user',
-        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-    );
-    `;
-
-    testRes =  connect(query1);
-
-    const query2 = 'SELECT * FROM users;';
-
-    testRes =  connect(query2);
+    // const query1 = `CREATE TABLE IF NOT EXISTS users (
+    //     id INT AUTO_INCREMENT PRIMARY KEY,
+    //     photo VARCHAR(50) NOT NULL,
+    //     login VARCHAR(50) UNIQUE NOT NULL,
+    //     password VARCHAR(255) NOT NULL,
+    //     email VARCHAR(50) UNIQUE NOT NULL,
+    //     status ENUM ('admin', 'user') DEFAULT 'user',
+    //     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    // );
+    // `;
+    //
+    // testRes =  connect(query1);
+    //
+    // const query2 = 'SELECT * FROM users;';
+    //
+    // testRes =  connect(query2);
 
 }
 
