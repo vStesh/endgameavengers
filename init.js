1343691f8ccc3ce67c32f7c758f6b401ff460ce7
Vladimir <stesh@ukr.net>
2021-10-07 12:10:07 +0300

    add users table

--- init.js
+++ init.js
@@ -21,12 +21,35 @@ const connect = (query) => {
 
   const test = 'SHOW TABLES;';
 
-const testRes = connect(test);
-if(testRes) {
-    console.log('БД подключена');
-    console.log()
-} else {
-    console.log('Ошибка подключения к БД')
+const start = async () => {
+
+    let testRes = await connect(test);
+    if(testRes) {
+        console.log('БД подключена');
+        console.log()
+    } else {
+        console.log('Ошибка подключения к БД')
+    }
+
+    const query1 = `CREATE TABLE IF NOT EXISTS users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        photo VARCHAR(50) NOT NULL,
+        login VARCHAR(50) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        email VARCHAR(50) UNIQUE NOT NULL,
+        status ENUM ('admin', 'user') DEFAULT 'user',
+        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    );
+    `;
+
+    testRes = await connect(query1);
+
+    const query2 = 'SELECT * FROM users;';
+
+    testRes = await connect(query2);
+
 }
+
+
 
 
