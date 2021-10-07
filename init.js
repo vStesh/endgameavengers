3ecd1ea0c80f45d2cbf95d4638a1218bbb07e5bf
Vladimir <stesh@ukr.net>
2021-10-07 11:54:09 +0300

    add init

---
+++ init.js
@@ -0,0 +1,30 @@
+const mysql = require("mysql2");
+const config = require('./config/config')();
+
+const connect = (query) => {
+    const connection = mysql.createConnection({
+        host     : config.dbtest ? config.db_test.host : (process.env.DB_HOST || config.db.host),
+        user     : config.dbtest ? config.db_test.user : (process.env.DB_USER || config.db.user),
+        password : config.dbtest ? config.db_test.password : (process.env.DB_PASSWORD || config.db.password),
+        database : config.dbtest ? config.db_test.name : (process.env.DB_NAME || config.db.name)
+    });
+    connection.connect();
+    let result;
+    try {
+        result = this.connection.promise().query(query);
+    } catch (err) {
+        console.error(err)
+    }
+    this.connection.end();
+    return result;
+  }
+
+  const test = 'SHOW TABLES;';
+
+if(connect(test)) {
+    console.log('БД подключена')
+} else {
+    console.log('Ошибка подключения к БД')
+}
+
+
